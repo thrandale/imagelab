a8b5a7710ab490b5367dbc92632721add0caf75c
Theo Crandall <thrandale@gmail.com>
2021-10-12 15:37:22 -0400

    Fixed Checkstyle errors in imagelab.java

--- imagelab/ImageLab.java
+++ imagelab/ImageLab.java
@@ -188,7 +188,8 @@ public class ImageLab {
                             "imagelab.ImageFilter");
                     } //for ja
                     if (isFilter) {
-                        ifilter = (ImageFilter) cl.getDeclaredConstructor().newInstance();
+                        ifilter = (ImageFilter) cl.getDeclaredConstructor()
+                            .newInstance();
                         filters.add(ifilter);
                         JMenuItem jmi = new JMenuItem(ifilter.getMenuLabel());
                         filter.add(jmi);
@@ -263,7 +264,8 @@ public class ImageLab {
         return new ActionListener() {
             public void actionPerformed(final ActionEvent ev) {
                 if (impro == null) {
-                    JOptionPane.showMessageDialog(myframe, "You must first select an image");
+                    JOptionPane.showMessageDialog(
+                        myframe, "You must first select an image");
                     return;
                 }
                 //System.out.println("Using impro number " + impro.getid());
@@ -285,7 +287,8 @@ public class ImageLab {
             public void actionPerformed(final ActionEvent e) {
                 ImgProvider improvider; // Hold the image
                 FileDialog fd;
-                fd = new FileDialog(frame, "Pick an image", FileDialog.LOAD);
+                fd = new FileDialog(
+                    frame, "Pick an image", FileDialog.LOAD);
                 fd.setVisible(true);
                 String theFile = fd.getFile();
                 String theDir = fd.getDirectory();
@@ -332,7 +335,8 @@ public class ImageLab {
                 //The imgProvider holding the image
                 ImgProvider improvider = impro;
                 if (improvider == null) {
-                    JOptionPane.showMessageDialog(myframe, "First select the image to play");
+                    JOptionPane.showMessageDialog(
+                        myframe, "First select the image to play");
                     return;
                 } //if
                 improvider.play();
@@ -351,7 +355,8 @@ public class ImageLab {
             public void actionPerformed(final ActionEvent e) {
                 ImgProvider improvider = impro; // Hold the image.
                 if (improvider == null) {
-                    JOptionPane.showMessageDialog(myframe, "Select the image to save");
+                    JOptionPane.showMessageDialog(
+                        myframe, "Select the image to save");
                     return;
                 } //if
                 improvider.save();
